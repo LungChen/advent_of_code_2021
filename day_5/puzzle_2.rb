8fcd500f31258142cb4db0042b490679715efb6c
Lyla Kao <lyla.kao@aktsk.com>
2021-12-05 15:54:40 +0800

    day 5 puzzles refactor

--- day_5/puzzle_2.rb
+++ day_5/puzzle_2.rb
@@ -7,37 +7,21 @@ File.open("#{dir}/input.txt").each_line do |input_line|
   line = input_line.split('->')
   p1 = line[0].strip.split(',').map(&:to_i)
   p2 = line[1].strip.split(',').map(&:to_i)
-  # puts "#{p1}, #{p2}"
 
-  direct_x = p1[0] < p2[0] ? 'right' : 'left'
-  direct_y = p1[1] < p2[1] ? 'down' : 'up'
+  distance_x = p2[0] - p1[0]
+  distance_y = p2[1] - p1[1]
 
-  if p1[0] == p2[0]
-    # y changed
-    distance = (p2[1] - p1[1]).abs + 1
-    distance.times do |i|
-      y = direct_y == 'down' ? p1[1] + i : p1[1] - i
-      array[p1[0]] = [] if array[p1[0]].nil?
-      array[p1[0]][y] = (array[p1[0]][y] || 0) + 1
-    end
-  elsif p1[1] == p2[1]
-    # x changed
-    distance = (p2[0] - p1[0]).abs + 1
-    distance.times do |i|
-      x = direct_x == 'right' ? p1[0] + i : p1[0] - i
-      array[x] = [] if array[x].nil?
-      array[x][p1[1]] = (array[x][p1[1]] || 0) + 1
-    end
-  else
-    # diagonal lines
-    distance = (p2[0] - p1[0]).abs + 1
-    distance.times do |i|
-      x = direct_x == 'right' ? p1[0] + i : p1[0] - i
-      y = direct_y == 'down' ? p1[1] + i : p1[1] - i
+  # next unless distance_x.zero? || distance_y.zero?
 
-      array[x] = [] if array[x].nil?
-      array[x][y] = (array[x][y] || 0) + 1
-    end
+  unit_x = distance_x.zero? ? 0 : distance_x / distance_x.abs
+  unit_y = distance_y.zero? ? 0 : distance_y / distance_y.abs
+
+  distance = [distance_x.abs, distance_y.abs].max + 1
+  distance.times do |i|
+    x = p1[0] + i * unit_x
+    y = p1[1] + i * unit_y
+    array[x] = [] if array[x].nil?
+    array[x][y] = (array[x][y] || 0) + 1
   end
 end
 
